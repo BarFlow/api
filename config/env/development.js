6cefbe79f0c02b13b158497610a7cfa02eef323d
Peter G <pepegombos@gmail.com>
2016-09-27 16:39:42 +0200

    feat(image upload), use environment vars for config

--- config/env/development.js
+++
@@ -1,9 +0,0 @@
-// Set db
-const host = process.env.BARFLOW_MONGO_HOST || 'localhost';
-
-export default {
-  env: 'development',
-  jwtSecret: '0a6b944d-d2fb-46fc-a85e-0295c986cd9f',
-  db: `mongodb://${host}/barflow-api-development`,
-  port: 3000
-};
