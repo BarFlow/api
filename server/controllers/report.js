1729398566804e56ff9bf136c1a43a6fee77fcdf
Peter Gombos <pepegombos@gmail.com>
2017-01-26 21:47:30 +0100

    report export now generates formatted XLS with supplier based order sheets, and order values

--- server/controllers/report.js
+++ server/controllers/report.js
@@ -3,6 +3,8 @@ import xl from 'excel4node';
 import _ from 'lodash';
 import Report from '../models/report';
 import Placement from '../models/placement';
+import Venue from '../models/venue';
+import User from '../models/user';
 import patchModel from '../helpers/patchModel';
 
 /**
@@ -104,87 +106,206 @@ function generateReport(filters) {
 }
 
 function getExport(req, res) {
-  const xls = generateXLSfromReport(req.report.data);
-  xls.write(`${new Date(req.report.created_at)
-    .toString()
-    .split(' ')
-    .splice(0, 5)
-    .join(' ')}.xlsx`, res);
+  Venue.findById(req.report.venue_id).execAsync().then(venue =>
+    User.findById(req.user._id).execAsync().then((user) => {
+      const xls = generateReportXLS(req.report.data, user, venue);
+      return xls.write(`${new Date(req.report.created_at)
+        .toString()
+        .split(' ')
+        .splice(0, 5)
+        .join(' ')}.xlsx`, res);
+    })
+  );
 }
 
-function generateXLSfromReport(report) {
-  let productNameMaxLength = 10;
-  let categoryMaxLength = 10;
-  let supplierNameMaxLength = 10;
-
-  const products = _.orderBy(report.map((item) => {
-    const supplier = item.supplier_id || {};
-    if (supplier.name && supplier.name.length > supplierNameMaxLength) {
-      supplierNameMaxLength = supplier.name.length;
-    }
-
-    if (item.product_id.name.length > productNameMaxLength) {
-      productNameMaxLength = item.product_id.name.length;
-    }
-
-    let category = item.product_id.category || 'other';
-    if (item.product_id.sub_category) {
-      category += `/${item.product_id.sub_category}`;
-    }
-    if (category.length > categoryMaxLength) {
-      categoryMaxLength = category.length;
-    }
-
-    return {
-      category,
-      name: item.product_id.name,
-      supplier: supplier.name || '',
-      sku: item.supplier_product_code || '',
-      par_level: item.par_level || 0,
-      stock_level: parseFloat(item.volume.toFixed(1)) || 0,
-      order: item.order || 0
-    };
-  }), ['category', 'supplier', 'name']);
-
-  // Create a new instance of  a Workbook class
-  const wb = new xl.Workbook();
-
+function addProductSheetToWorkBook(wb, products, user, venue, supplier) {
   // Add Worksheets to the workbook
-  const ws = wb.addWorksheet('Stock Sheet');
+  const ws = wb.addWorksheet(supplier.name || 'Other');
 
+  // Calculate longest product name's length
+  let productNameMaxLength = 10;
+  products.forEach((item) => {
+    if (item.name.length > productNameMaxLength) {
+      productNameMaxLength = item.name.length;
+    }
+  });
+
+  // Set column widths
+  ws.column(1).setWidth(12);
+  ws.column(2).setWidth(productNameMaxLength);
+  ws.column(7).setWidth(24);
+  ws.column(8).setWidth(18);
+
+
+  // Styles
   const header = wb.createStyle({
     font: {
       bold: true,
     },
     alignment: {
+      horizontal: 'left'
+    },
+    fill: {
+      type: 'pattern',
+      patternType: 'solid',
+      fgColor: '#FFFF00'
+    }
+  });
+
+  const yellowBg = wb.createStyle({
+    fill: {
+      type: 'pattern',
+      patternType: 'solid',
+      fgColor: '#FFFF00'
+    }
+  });
+
+  const supplierName = wb.createStyle({
+    fill: {
+      type: 'pattern',
+      patternType: 'solid',
+      fgColor: '#383838'
+    },
+    alignment: {
+      horizontal: 'center'
+    },
+    font: {
+      bold: true,
+      color: '#FFFFFF',
+      size: 14
+    }
+  });
+
+  const header2 = wb.createStyle({
+    font: {
+      bold: true,
+    },
+    alignment: {
+      horizontal: 'center'
+    },
+    fill: {
+      type: 'pattern',
+      patternType: 'solid',
+      fgColor: '#ececec'
+    }
+  });
+
+  const currencyStyle = wb.createStyle({
+    numberFormat: '£#,##0.00; (£#,##0.00); -'
+  });
+
+  const alignRight = wb.createStyle({
+    alignment: {
+      horizontal: 'right'
+    }
+  });
+
+  const alignCenter = wb.createStyle({
+    alignment: {
       horizontal: 'center'
     }
   });
 
+  // Top header has 4 rows
+  const skipRows = 5;
+  const currentDate = new Date();
+
+  // Top header
+  ws.cell(1, 1, 1, 8, true).string(supplier.name || 'Other').style(supplierName);
+  ws.cell(2, 1, 5, 8).style(yellowBg);
+  ws.cell(2, 1).string('Name:').style(header);
+  ws.cell(2, 2).string(venue.profile.name || '');
+  ws.cell(2, 7).string('Date:').style(header);
+  ws.cell(2, 8).date(currentDate);
+  ws.cell(3, 1).string('Account:').style(header);
+  ws.cell(3, 2).string(supplier.account_number || '');
+  ws.cell(3, 7).string('Contact:').style(header);
+  ws.cell(3, 8).string(venue.profile.tel || '').style(alignRight);
+  ws.cell(4, 1).string('Address:').style(header);
+  ws.cell(4, 2).string(venue.profile.address || '');
+  ws.cell(4, 7).string('Order Placed By:').style(header);
+  ws.cell(4, 8).string(user.name || '').style(alignRight);
+  ws.cell(5, 1).string('Email:').style(header);
+  ws.cell(5, 2).string(venue.profile.email || '');
+  ws.cell(5, 7).string('Requested Delivery Date:').style(header);
+  ws.cell(5, 8).date(currentDate);
+
+  // Columns definition
   const columns = [
-    { key: 'category', title: 'Category', type: 'string' },
-    { key: 'name', title: 'Product', type: 'string' },
-    { key: 'supplier', title: 'Supplier', type: 'string' },
-    { key: 'sku', title: 'SKU', type: 'string' },
-    { key: 'par_level', title: 'Par Level', type: 'number' },
-    { key: 'stock_level', title: 'Stock Level', type: 'number' },
-    { key: 'order', title: 'Order', type: 'number' }
+    { key: 'sku', title: 'SKU', type: 'string' }, // A
+    { key: 'name', title: 'Description', type: 'string' }, // B
+    { key: 'cost_price', title: 'Net Price', type: 'number' }, // C
+    { key: 'inc_vat', title: 'Inc VAT', type: 'number' }, // D
+    { key: 'par_level', title: 'Par Level', type: 'number' }, // E
+    { key: 'stock_level', title: 'Stock Level', type: 'number' }, // F
+    { key: 'order', title: 'Order Quantity (Bottles)', type: 'number' }, // G
+    { key: 'value', title: 'Value (Inc VAT)', type: 'number' } // H
   ];
 
+  // Freeze Header BUG: https://github.com/amekkawi/excel4node/issues/107
+  // ws.row(5).freeze();
+
+  // List products
   for (let i = 0; i < products.length; i++) {
     if (i === 0) {
       for (let k = 0; k < columns.length; k++) {
-        ws.cell(i + 1, k + 1).string(columns[k].title).style(header);
+        ws.cell(i + 1 + skipRows, k + 1).string(columns[k].title).style(header2);
       }
     }
     for (let j = 0; j < columns.length; j++) {
-      ws.cell(i + 2, j + 1)[columns[j].type](products[i][columns[j].key]);
+      const row = i + 2 + skipRows;
+      if (columns[j].key === 'inc_vat') {
+        ws.cell(row, j + 1).formula(`C${row}*1.2`).style(currencyStyle);
+      } else if (columns[j].key === 'value') {
+        ws.cell(row, j + 1).formula(`G${row}*D${row}`).style(currencyStyle);
+      } else {
+        ws.cell(row, j + 1)[columns[j].type](products[i][columns[j].key]);
+        if (columns[j].key === 'cost_price') {
+          ws.cell(row, j + 1).style(currencyStyle);
+        }
+      }
     }
   }
 
-  ws.column(1).setWidth(categoryMaxLength);
-  ws.column(2).setWidth(productNameMaxLength);
-  ws.column(3).setWidth(supplierNameMaxLength);
+  // Claculate total cost for the order
+  const lastRow = products.length + skipRows + 2;
+  ws.cell(lastRow, 1, lastRow, 8).style(header2).style(alignRight);
+  ws.cell(lastRow, 7).string('Total (Inc VAT)');
+  ws.cell(lastRow, 8).formula(`SUM(H6:H${(lastRow - 1)})`).style(currencyStyle);
+
+  // Barflow branding
+  ws.cell(lastRow + 1, 1, lastRow + 1, 8, true).string('Generated by BarFlow').style(alignCenter);
+  ws.cell(lastRow + 2, 1, lastRow + 2, 8, true).link('http://barflow.io').style(alignCenter);
+
+  // Return the workbook with the new sheet
+  return wb;
+}
+
+function generateReportXLS(report, user, venue) {
+  // Create a new instance of  a Workbook class
+  let wb = new xl.Workbook({
+    dateFormat: 'dd.mm.yyyy;@'
+  });
+
+  const productsBySuppliers = _.groupBy(report, 'supplier_id._id');
+
+  Object.keys(productsBySuppliers).forEach((supplierId) => {
+    const products = productsBySuppliers[supplierId].map(item => (
+      {
+        name: item.product_id.name,
+        sku: item.supplier_product_code || '',
+        par_level: item.par_level || 0,
+        cost_price: item.cost_price || 0,
+        supplier: item.supplier || {},
+        stock_level: parseFloat(item.volume.toFixed(1)) || 0,
+        order: item.order || 0
+      }
+    ));
+
+    const supplier = productsBySuppliers[supplierId][0].supplier_id || {};
+
+    wb = addProductSheetToWorkBook(wb, products, user, venue, supplier);
+  });
 
   return wb;
 }
